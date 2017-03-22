6b7cd9719c17bea2ff566abda05bc425ed35e075
c_youwu <c_youwu@qti.qualcomm.com>
2017-03-22 10:28:02 +0800

    add

--- test_tmp.py
+++ test_tmp.py
@@ -7,7 +7,7 @@ Utility.stop_process_on_device(package_name)
 Utility.stop_process_on_device('uiautomator')
 Utility.push_file_to_device(local=GlobalVariable.uiautomator_jar, remote='/data/local/tmp/')
 Utility.run_command_on_pc('adb root')
-
+import time
 import os
 from subprocess import Popen
 from subprocess import PIPE
@@ -19,23 +19,23 @@ count = 0
 for line in iter(a.stdout.readline, ''):
     if 'STATE:WAIT' in line:
         count += 1
-        print count
         if not thread or not thread.is_alive():
             thread = ApplicationClassification()
             thread.start()
-        print thread.is_alive()
+        print time.time()
+
         # tmp_xml = os.path.join(abs_path, 'logs', 'tmp', '%s.xml' % count)
         # tmp_txt = os.path.join(abs_path, 'logs', 'tmp', '%s.txt' % count)
         # os.system('adb pull /data/local/tmp/current.xml %s' % tmp_xml)
         # ApplicationClassification.analysis_dump(tmp_xml)
         #os.system('adb push C:\Users\c_youwu\Desktop\UiTest\\tmp\\tmp.txt /data/local/tmp/Action.txt')
         #os.system('adb push %s /data/local/tmp/Action.txt' % tmp_txt)
-    elif 'STATE:DumpException' in line:
-        print '**************************************'
-    elif 'STATE:UiObjectNotFoundException' in line:
-        print '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
-    else:
-        pass
-    if count % 10 ==0:
-        pass
+    elif 'STATE:' in line:
+        print line
+    # elif 'STATE:UiObjectNotFoundException' in line:
+    #     print '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+    # else:
+    #     pass
+    # if count % 10 ==0:
+    #     pass
 
