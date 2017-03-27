4fce05961ca8c7731250980976f9a527a770799e
c_youwu <c_youwu@qti.qualcomm.com>
2017-03-27 11:23:26 +0800

    add

--- libs/DebugMain.py
+++ libs/DebugMain.py
@@ -1,13 +1,24 @@
 __author__ = 'c_youwu'
 from UiAutomator import UiAutomator
+from Utility import Utility
+from GlobalVariable import GlobalVariable
+from os.path import join
 
 class Debug(object):
     def __init__(self, package_name, serial=None):
         self.package_name = package_name
         self.device = UiAutomator(serial)
+        self.logs_directory = Utility.make_dirs(join(GlobalVariable.logs_directory, package_name))
+        self.
 
     def main(self):
         print self.device.get_current_package_name()
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
