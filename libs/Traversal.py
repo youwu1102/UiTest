860dc4f97450acb9024b5d8d86ae5b80591a94f7
c_youwu <c_youwu@qti.qualcomm.com>
2017-04-07 13:59:13 +0800

    add

--- libs/Traversal.py
+++ libs/Traversal.py
@@ -1,8 +1,14 @@
 __author__ = 'c_youwu'
 
-class Traversal(object):
-    def __init__(self):
-        pass
+
+
+class TraversalNode(object):
+    def __init__(self,eigenvalue):
+        self.__eigenvalue = eigenvalue
+        self.__open = []
+        self.__closed = []
+        self.__previous = []
+        self.__next = []
 
     def append_previous(self):
         pass
@@ -10,9 +16,23 @@ class Traversal(object):
     def append_next(self):
         pass
 
-    def open(self):
+    def init_open(self):
         pass
 
-    def closed(self):
+    def move_to_closed(self, element):
         pass
 
+    def get_open(self):
+        return self.__open
+
+    def get_closed(self):
+        return self.__closed
+
+    def get_node_eigenvalue(self):
+        return self.__eigenvalue
+
+    def get_previous(self):
+        return self.__previous
+
+    def get_next(self):
+        return self.__next
